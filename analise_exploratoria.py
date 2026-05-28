183b50133137b07dc11a2f7ce71c24a7edd1e8f6
luizcfreitasneto <luizcfreitasneto@gmail.com>
2026-05-28 18:35:25 -0300

    primeiro commit

---
+++ analise_exploratoria.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import csv
+
+# carregar dados do arquivo Base Varejo.csv (usar Dict Reader)
+# exibir primeiras linhas do Data Frame
+# exibir dimensões do Data Frame
+# exibir informações gerais do Data Frame
+# exibir tipos de dados das colunas
+# exibir valores nulos por coluna
